9a1005da1c8e5a17c9bc610a279e2012ae3123f7
Enoch <enochkpoti@gmail.com>
2020-07-07 11:03:38 +0000

    changed button color

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6Lc8rrIUAAAAAN5xp0domm9PZj_6qGZU2LTDsMLh"); 
+define('SECRET_KEY',"6Lc8rrIUAAAAABTM8IyrJemSGufWjfHArJSOo-ln");
+?>
